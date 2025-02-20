6ebf4765369a7f90cb7da0c19c8e84c02f468486
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-02-20 21:02:59 +0100

    correct and wrong answers

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let questionNumber = 0;
+let counter = 0;
 
 function init() {
     getFromLocalStorage();
@@ -17,13 +19,37 @@ function getFromLocalStorage() {
 
 function loadFirstQuestion() {
     let questionRef = document.getElementById("question");
-    questionRef.innerText = questions[0].question;
+    questionRef.innerText = questions[questionNumber].question;
     let answerOneRef = document.getElementById("answer_1");
-    answerOneRef.innerText = questions[0].answer_1;
+    answerOneRef.innerText = questions[questionNumber].answer_1;
     let answerTwoRef = document.getElementById("answer_2");
-    answerTwoRef.innerText = questions[0].answer_2;
+    answerTwoRef.innerText = questions[questionNumber].answer_2;
     let answerThreeRef = document.getElementById("answer_3");
-    answerThreeRef.innerText = questions[0].answer_3;
+    answerThreeRef.innerText = questions[questionNumber].answer_3;
     let answerFourRef = document.getElementById("answer_4");
-    answerFourRef.innerText = questions[0].answer_4;
+    answerFourRef.innerText = questions[questionNumber].answer_4;
+}
+
+function nextQuestion() {
+    document.getElementById("answer_1").parentNode.classList.remove("bg_green", "bg_red");
+    document.getElementById("answer_2").parentNode.classList.remove("bg_green", "bg_red");
+    document.getElementById("answer_3").parentNode.classList.remove("bg_green", "bg_red");
+    document.getElementById("answer_4").parentNode.classList.remove("bg_green", "bg_red");
+    questionNumber++;
+    loadFirstQuestion();
+}
+
+function selectedAnswer(num) {
+    let selectetAnswer = document.getElementById("answer_" + num);
+    
+    if(questions[questionNumber].correctAnswer == num) {
+        selectetAnswer.parentNode.classList.add("bg_green");
+        counter++;
+        console.log(counter);
+        
+    } else{
+        selectetAnswer.parentNode.classList.add("bg_red");
+        let correctAnswer = document.getElementById("answer_" + questions[questionNumber].correctAnswer);
+        correctAnswer.parentNode.classList.add("bg_green");
+    }
 }
