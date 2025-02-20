27dbd21c281b821d1d39aac4d68c23205c58dc66
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-02-20 17:35:23 +0100

    correct database and render first question

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 function init() {
     getFromLocalStorage();
+    loadFirstQuestion();
 }
 
 function saveToLocalStorage(){
@@ -12,4 +13,17 @@ function getFromLocalStorage() {
     if(myHighscores != null) {
         highscores = myHighscores;        
     }
+}
+
+function loadFirstQuestion() {
+    let questionRef = document.getElementById("question");
+    questionRef.innerText = questions[0].question;
+    let answerOneRef = document.getElementById("answer_1");
+    answerOneRef.innerText = questions[0].answer_1;
+    let answerTwoRef = document.getElementById("answer_2");
+    answerTwoRef.innerText = questions[0].answer_2;
+    let answerThreeRef = document.getElementById("answer_3");
+    answerThreeRef.innerText = questions[0].answer_3;
+    let answerFourRef = document.getElementById("answer_4");
+    answerFourRef.innerText = questions[0].answer_4;
 }
