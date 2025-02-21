a21ff0ef3321b38da1a54f26392aa91797a152ff
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-02-21 07:04:29 +0100

    change between screens

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let counter = 0;
 
 function init() {
     getFromLocalStorage();
+    selectQuiz(1);
     loadFirstQuestion();
 }
 
@@ -15,6 +16,14 @@ function getFromLocalStorage() {
     if(myHighscores != null) {
         highscores = myHighscores;        
     }
+}
+
+function startQuiz() {
+    let startScreen = document.getElementById("quiz_start_screen");
+    startScreen.classList.add("d_none");
+
+    let questionScreen = document.getElementById("quiz_question_screen");
+    questionScreen.classList.remove("d_none");
 }
 
 function loadFirstQuestion() {
@@ -35,7 +44,11 @@ function nextQuestion() {
     document.getElementById("answer_2").parentNode.classList.remove("bg_green", "bg_red");
     document.getElementById("answer_3").parentNode.classList.remove("bg_green", "bg_red");
     document.getElementById("answer_4").parentNode.classList.remove("bg_green", "bg_red");
+    let disableBtn = document.getElementById("button_next");
+    disableBtn.disabled = true;
+    disableBtn.classList.toggle("btn_disabled");
     questionNumber++;
+    showEndScreen();
     loadFirstQuestion();
 }
 
@@ -51,5 +64,64 @@ function selectedAnswer(num) {
         selectetAnswer.parentNode.classList.add("bg_red");
         let correctAnswer = document.getElementById("answer_" + questions[questionNumber].correctAnswer);
         correctAnswer.parentNode.classList.add("bg_green");
+    }
+    let disableBtn = document.getElementById("button_next");
+    disableBtn.disabled = false;
+    disableBtn.classList.toggle("btn_disabled");
+}
+
+function showEndScreen() {
+    if(questionNumber == 10 || questionNumber == 20 || questionNumber == 30 || questionNumber == 40) {
+        showScore();
+
+        let questionScreen = document.getElementById("quiz_question_screen");
+        questionScreen.classList.add("d_none");
+
+        let endScreen = document.getElementById("quiz_end_screen");
+        endScreen.classList.remove("d_none");
+    }
+}
+
+function showScore() {
+    let currentScore = document.getElementById("score");
+    currentScore.innerHTML = counter + "/10";
+}
+
+function selectQuiz(num) {
+    let quizTitle = document.getElementById("quiz_title");
+
+    let htmlTheme = document.getElementById("html_theme");
+    let cssTheme = document.getElementById("css_theme");
+    let jsTheme = document.getElementById("js_theme");
+    let javaTheme = document.getElementById("java_theme");
+
+    if(num == 1) {
+        questionNumber = 0;
+        quizTitle.innerHTML = templateQuizHtml();
+        htmlTheme.classList.add("border_left");
+        cssTheme.classList.remove("border_left");
+        jsTheme.classList.remove("border_left");
+        javaTheme.classList.remove("border_left");
+    } if (num == 2) {
+        questionNumber = 10;
+        quizTitle.innerHTML = templateQuizCss();
+        htmlTheme.classList.remove("border_left");
+        cssTheme.classList.add("border_left");
+        jsTheme.classList.remove("border_left");
+        javaTheme.classList.remove("border_left");
+    } if (num == 3) {
+        questionNumber = 20;
+        quizTitle.innerHTML = templateQuizJs();
+        htmlTheme.classList.remove("border_left");
+        cssTheme.classList.remove("border_left");
+        jsTheme.classList.add("border_left");
+        javaTheme.classList.remove("border_left");
+    } if (num == 4) {
+        questionNumber = 30;
+        quizTitle.innerHTML = templateQuizJava();
+        htmlTheme.classList.remove("border_left");
+        cssTheme.classList.remove("border_left");
+        jsTheme.classList.remove("border_left");
+        javaTheme.classList.add("border_left");
     }
 }
---
+++ scripts/templates.js
@@ -0,0 +1,24 @@
+
+function templateQuizHtml() {
+    return `
+        Welcome to<br>The Awesome HTML Quiz
+    `;
+}
+
+function templateQuizCss() {
+    return `
+        Welcome to<br>The Awesome CSS Quiz
+    `;
+}
+
+function templateQuizJs() {
+    return `
+        Welcome to<br>The Awesome JavaScript Quiz
+    `;
+}
+
+function templateQuizJava() {
+    return `
+        Welcome to<br>The Awesome Java Quiz
+    `;
+}
